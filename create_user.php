5b3dd20a25e1db6bb1b332da3009ccfeac7f08eb
Kevin McKinney <kevinmckinney99@utexas.edu>
2020-11-11 13:15:35 -0600

    added CSS to sign up sheet

--- create_user.php
+++ create_user.php
@@ -17,13 +17,34 @@ function createUser(){
 	<meta charset="UTF-8">
 	<meta name="description" content="A User Registration Page">
     <meta name="author" content="Uzair Saleem">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="signup.css">
+    <link href="https://fonts.googleapis.com/css2?family=Play:wght@700&display=swap" rel="stylesheet">
+
 </head>
 <body>
+
+    <div id="header">
+            
+    <div id="title">
+        <a href="qgames-index.html" style="text-decoration:none"><h1>QUARANTINE GAMES</h1></a>
+    </div>
+
+    <div id="tabs">
+        <button class="tablink" onclick="location.href = 'sample.html';">Action</button>
+        <button class="tablink" onclick="location.href = 'sample.html';">Sports</button>
+        <button class="tablink" onclick="location.href = 'sample.html';">Adventure</button>
+        <button class="tablink" onclick="location.href = 'sample.html';">Puzzle</button>
+        <button class="tablink" onclick="location.href = 'sample.html';">Party</button>
+        <button class="tablink" id="last-tab" onclick="location.href = 'sample.html';">Strategy</button>
+    </div>
+
+    </div>
+
     <form action="$script" method="post" onsubmit="return validateUserGeneration()">
+        <h2>Sign Up</h2>
         <table>
             <tr>
-                <td id="words">Username</td>
+                <td id="words">Username:</td>
             </tr>
             <tr>
             <td><label><input id="username" name="user" type="text"></label></td>
@@ -32,16 +53,16 @@ function createUser(){
                 <td id="words">Password:</td>
             </tr>
             <tr>
-                <td><label><input id="password" name="pass" type="text"></label></td>
+                <td><label><input id="password" name="pass" type="password"></label></td>
             </tr>
             <tr>
                 <td id="words">Repeat Password:</td>
             </tr>
             <tr>
-                <td><label><input id="pass_repeat" name="repeat_pass" type="text"></label></td>
+                <td><label><input id="pass_repeat" name="repeat_pass" type="password"></label></td>
             </tr>
             <tr>
-                <td id="buttonRow"><input id="submitButton" type=submit name="created" value="Register">
+                <td id="buttonRow"><input id="submitButton" type=submit name="created" value="Submit">
                     <input id="resetButton" type=reset  value="Clear"></td>
             </tr>
         </table>    
