71434d6afd1f8addbd1588d1641d18a40e502629
thnjensen <71658103+thnjensen@users.noreply.github.com>
2020-12-06 18:34:45 -0600

    updated interest page

--- interest.php
+++ interest.php
@@ -11,7 +11,7 @@
 		} else {}
 		
 		if ($_POST['tba'] == 'Interested') {
-			$command = "SELECT num FROM games WHERE game = \"tba\"";
+			$command = "SELECT num FROM games WHERE game = 'Text Based Adventures'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -20,13 +20,13 @@
 				$num = $row["num"];
 			}
 			$num = $num + 1;
-			$sql = "UPDATE games SET num = " . $num . " WHERE game = \"tba\"";
+			$sql = "UPDATE games SET num = " . $num . " WHERE game = 'Text Based Adventures'";
 			$newresult = $mysqli->query($sql);
 			if (!$newresult) {
 				die("Query Failed: ($mysqli->error <br> SQL command = $sql");
 			}
 		} else if ($_POST['tba'] == 'Not Interested') {	
-			$command = "SELECT num FROM games WHERE game = \"tba\"";
+			$command = "SELECT num FROM games WHERE game = 'Text Based Adventures'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -36,14 +36,14 @@
 			}
 			if ($num != 0) {
 				$num = $num - 1;
-				$sql = "UPDATE games SET num = " . $num . " WHERE game = \"tba\"";
+				$sql = "UPDATE games SET num = " . $num . " WHERE game = 'Text Based Adventures'";
 				$newresult = $mysqli->query($sql);
 				if (!$newresult) {
 					die("Query Failed: ($mysqli->error <br> SQL command = $sql");
 				}
 			}
 		} else if ($_POST['league'] == 'Interested') {
-			$command = "SELECT num FROM games WHERE game = \"league\"";
+			$command = "SELECT num FROM games WHERE game = 'League of Legends'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -52,13 +52,13 @@
 				$num = $row["num"];
 			}
 			$num = $num + 1;
-			$sql = "UPDATE games SET num = " . $num . " WHERE game = \"league\"";
+			$sql = "UPDATE games SET num = " . $num . " WHERE game = 'League of Legends'";
 			$newresult = $mysqli->query($sql);
 			if (!$newresult) {
 				die("Query Failed: ($mysqli->error <br> SQL command = $sql");
 			}
 		} else if ($_POST['league'] == 'Not Interested') {	
-			$command = "SELECT num FROM games WHERE game = \"league\"";
+			$command = "SELECT num FROM games WHERE game = 'League of Legends'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -68,14 +68,14 @@
 			}
 			if ($num != 0) {
 				$num = $num - 1;
-				$sql = "UPDATE games SET num = " . $num . " WHERE game = \"league\"";
+				$sql = "UPDATE games SET num = " . $num . " WHERE game = ''League of Legends";
 				$newresult = $mysqli->query($sql);
 				if (!$newresult) {
 					die("Query Failed: ($mysqli->error <br> SQL command = $sql");
 				}
 			}
 		}else if ($_POST['runescape'] == 'Interested') {
-			$command = "SELECT num FROM games WHERE game = \"runescape\"";
+			$command = "SELECT num FROM games WHERE game = 'Runescape'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -84,13 +84,13 @@
 				$num = $row["num"];
 			}
 			$num = $num + 1;
-			$sql = "UPDATE games SET num = " . $num . " WHERE game = \"runescape\"";
+			$sql = "UPDATE games SET num = " . $num . " WHERE game = 'Runescape'";
 			$newresult = $mysqli->query($sql);
 			if (!$newresult) {
 				die("Query Failed: ($mysqli->error <br> SQL command = $sql");
 			}
 		} else if ($_POST['runescape'] == 'Not Interested') {	
-			$command = "SELECT num FROM games WHERE game = \"runescape\"";
+			$command = "SELECT num FROM games WHERE game = 'Runescape'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -100,14 +100,14 @@
 			}
 			if ($num != 0) {
 				$num = $num - 1;
-				$sql = "UPDATE games SET num = " . $num . " WHERE game = \"runescape\"";
+				$sql = "UPDATE games SET num = " . $num . " WHERE game = 'Runescape'";
 				$newresult = $mysqli->query($sql);
 				if (!$newresult) {
 					die("Query Failed: ($mysqli->error <br> SQL command = $sql");
 				}
 			}
 		}else if ($_POST['mines'] == 'Interested') {
-			$command = "SELECT num FROM games WHERE game = \"mines\"";
+			$command = "SELECT num FROM games WHERE game = 'Minesweeper'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -116,13 +116,13 @@
 				$num = $row["num"];
 			}
 			$num = $num + 1;
-			$sql = "UPDATE games SET num = " . $num . " WHERE game = \"mines\"";
+			$sql = "UPDATE games SET num = " . $num . " WHERE game = 'Minesweeper'";
 			$newresult = $mysqli->query($sql);
 			if (!$newresult) {
 				die("Query Failed: ($mysqli->error <br> SQL command = $sql");
 			}
 		} else if ($_POST['mines'] == 'Not Interested') {	
-			$command = "SELECT num FROM games WHERE game = \"mines\"";
+			$command = "SELECT num FROM games WHERE game = 'Minesweeper'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -132,14 +132,14 @@
 			}
 			if ($num != 0) {
 				$num = $num - 1;
-				$sql = "UPDATE games SET num = " . $num . " WHERE game = \"mines\"";
+				$sql = "UPDATE games SET num = " . $num . " WHERE game = 'Minesweeper'";
 				$newresult = $mysqli->query($sql);
 				if (!$newresult) {
 					die("Query Failed: ($mysqli->error <br> SQL command = $sql");
 				}
 			}
 		} else if ($_POST['picapix'] == 'Interested') {
-			$command = "SELECT num FROM games WHERE game = \"picapix\"";
+			$command = "SELECT num FROM games WHERE game = 'Pic-a-Pix'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -148,13 +148,13 @@
 				$num = $row["num"];
 			}
 			$num = $num + 1;
-			$sql = "UPDATE games SET num = " . $num . " WHERE game = \"picapix\"";
+			$sql = "UPDATE games SET num = " . $num . " WHERE game = 'Pic-a-Pix'";
 			$newresult = $mysqli->query($sql);
 			if (!$newresult) {
 				die("Query Failed: ($mysqli->error <br> SQL command = $sql");
 			}
 		} else if ($_POST['picapix'] == 'Not Interested') {	
-			$command = "SELECT num FROM games WHERE game = \"picapix\"";
+			$command = "SELECT num FROM games WHERE game = 'Pic-a-Pix'";
 			$result = $mysqli->query($command);
 			if (!$result) {
 				die("Query failed: ($mysqli->error <br> SQL command = $command");
@@ -164,7 +164,7 @@
 			}
 			if ($num != 0) {
 				$num = $num - 1;
-				$sql = "UPDATE games SET num = " . $num . " WHERE game = \"picapix\"";
+				$sql = "UPDATE games SET num = " . $num . " WHERE game = 'Pic-a-Pix'";
 				$newresult = $mysqli->query($sql);
 				if (!$newresult) {
 					die("Query Failed: ($mysqli->error <br> SQL command = $sql");
